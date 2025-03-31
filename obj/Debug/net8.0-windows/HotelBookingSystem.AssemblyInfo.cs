b414bed3fe5f48e0533e82234cc8d2a026e2bddb
nhaserey <sereynha473@gmail.com>
2025-04-01 00:02:02 +0700

    update

--- obj/Debug/net8.0-windows/HotelBookingSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/HotelBookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c743eb368563ba6ed40dff6e5317f59eba7f00")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
